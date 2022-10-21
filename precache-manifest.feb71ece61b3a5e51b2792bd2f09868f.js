2d83a42f025d55107045d4542a0c036b6be5756c
Imakashkendre <akash.shopisthan@gmail.com>
2022-10-21 09:51:23 +0530

    Updates

---
+++ precache-manifest.feb71ece61b3a5e51b2792bd2f09868f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc106467b8b391ff0912",
+    "url": "/Restaurant/static/css/main.64fabefd.chunk.css"
+  },
+  {
+    "revision": "cc106467b8b391ff0912",
+    "url": "/Restaurant/static/js/main.cc106467.chunk.js"
+  },
+  {
+    "revision": "4f473d823a9b02f8d234",
+    "url": "/Restaurant/static/js/1.4f473d82.chunk.js"
+  },
+  {
+    "revision": "e476af72bcdd0ae16035",
+    "url": "/Restaurant/static/js/2.e476af72.chunk.js"
+  },
+  {
+    "revision": "241d4d968ab530844e45",
+    "url": "/Restaurant/static/js/runtime~main.241d4d96.js"
+  },
+  {
+    "revision": "c934905ba26876efc78e6c17b4410c75",
+    "url": "/Restaurant/static/media/greater than.c934905b.png"
+  },
+  {
+    "revision": "3cd8f87f4a2f3857887439bab1ba240a",
+    "url": "/Restaurant/static/media/banner-1.3cd8f87f.jpg"
+  },
+  {
+    "revision": "716c23c93651ac45328c4f9188219dc1",
+    "url": "/Restaurant/static/media/banner-2.716c23c9.jpg"
+  },
+  {
+    "revision": "b68d0922b775ed0f74600aec24353fe2",
+    "url": "/Restaurant/static/media/zoom1.b68d0922.jpg"
+  },
+  {
+    "revision": "991654070325e8156a149aca116a205a",
+    "url": "/Restaurant/index.html"
+  }
+];
